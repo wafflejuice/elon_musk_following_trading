5a016e839888e4385ea6b74c5b06903c9f54adf8
wafflejuice <wafflejuice96@gmail.com>
2021-02-05 23:08:01 +0900

    initial commit

---
+++ constants.py
@@ -0,0 +1,41 @@
+EPOCH_TIME_DAY_MS = 86400000
+EPOCH_TIME_HOUR_MS = 3600000
+EPOCH_TIME_MINUTE_MS = 60000
+EPOCH_TIME_SECOND_MS = 1000
+
+# ------------------------------------
+
+BINANCE_NAME = 'BINANCE'
+FUTURES_NAME = 'FUTURES'
+
+# ------------------------------------
+
+USDT_SYMBOL = 'USDT'
+
+DOGE_SYMBOL = 'DOGE'
+DOGE_USDT = 'DOGEUSDT'
+DOGE_SLASH_USDT = 'DOGE/USDT'
+
+BTC_SYMBOL = 'BTC'
+BTC_USDT = 'BTCUSDT'
+BTC_SLASH_USDT = 'BTC/USDT'
+
+# ------------------------------------
+
+DOGE_KEYWORDS = ['doge',]
+BTC_KEYWORDS = ['bitcoin', 'btc', 'xbt', '₿',]
+OTHER_KEYWORDS = ['coin', 'satoshi', 'crypto']
+
+# ------------------------------------
+
+BINANCE_MAKER_FEE_RATIO = 0.001
+BINANCE_TAKER_FEE_RATIO = 0.001
+
+# ------------------------------------
+
+TELEGRAM_GET_UPDATES_BASE_URL = 'https://api.telegram.org/bot{}/getUpdates'
+TELEGRAM_SEND_MESSAGE_BASE_URL = 'https://api.telegram.org/bot{}/sendMessage'
+
+# ------------------------------------
+
+ELON_MUSK_TWITTER_ID = '44196397'
