c75693c71255405241a0d9d5911609ddb9e0d181
wafflejuice <wafflejuice96@gmail.com>
2021-03-11 21:21:49 +0900

    twitter id : config -> constants

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ from exchange import Coin
 from twitter import Twitter
 from config import Config
 import logger
+import constants
 
 class App:
 	@staticmethod
 	def run():
 		config = Config.load_config()
-		elon_musk_twitter_id = config['twitter']['twitter id']
 		price_update_period = config['binance']['period']
 		
 		Coin().update_price_periodical(price_update_period)
 		
 		while True:
 			try:
-				logger.logger.info('connect to '+elon_musk_twitter_id+'...')
+				logger.logger.info('connect to '+constants.ELON_MUSK_TWITTER_ID+'...')
 				
 				streaming_api = tweepy.streaming.Stream(Twitter().oauth1a, Twitter.CustomStreamListener(), timeout=60)
-				streaming_api.filter(follow=[elon_musk_twitter_id])
+				streaming_api.filter(follow=[constants.ELON_MUSK_TWITTER_ID])
 				
 			except tweepy.TweepError as e:
 				logger.logger.error(e)
